1f30bab0f11bef9388a798f5c76b9cbbedf13259
Jeff Stahlnecker <45363541+jeffstahlnecker@users.noreply.github.com>
2021-02-04 03:38:17 +0100

    docs: remove docusaurus logo
    Made the docusaurus logo less apparent in the docs navbar

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -10,21 +10,14 @@ module.exports = {
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
+      title: 'Gamz Documentation',
+      // logo: {
+      //   alt: 'My Site Logo',
+      //   src: 'img/logo.svg',
+      // },
       items: [
         {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
-        },
-        {to: 'blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/jeffstahlnecker/gamz',
           label: 'GitHub',
           position: 'right',
         },
@@ -89,12 +82,6 @@ module.exports = {
           // Please change this to your repo.
           editUrl:
             'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
